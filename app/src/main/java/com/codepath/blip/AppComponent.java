569d60bf80c57db90a76860df0ee35c3e8c29d26
Zach Boline <zachb@uber.com>
2016-08-28 20:07:14 -0700

    Yo

--- app/src/main/java/com/codepath/blip/AppComponent.java
+++ app/src/main/java/com/codepath/blip/AppComponent.java
@@ -1,5 +1,6 @@
 package com.codepath.blip;
 
+import com.codepath.blip.fragments.UserBlipFragment;
 import com.codepath.blip.modules.BackendModule;
 
 import javax.inject.Singleton;
@@ -15,4 +16,5 @@ import dagger.Component;
 @Component(modules = {BackendModule.class})
 public interface AppComponent {
     void inject(MainActivity activity);
+    void inject(UserBlipFragment fragment);
 }
