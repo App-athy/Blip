cb34b1482e9150068c65c092fad5013b7602dcea
Zach Boline <zachb@uber.com>
2016-08-28 20:47:02 -0700

    toast

--- app/src/main/java/com/codepath/blip/AppComponent.java
+++ app/src/main/java/com/codepath/blip/AppComponent.java
@@ -18,5 +18,4 @@ public interface AppComponent {
     void inject(MainActivity activity);
     void inject(UserBlipFragment fragment);
     void inject(LoginActivity activity);
-    void inject(UserBlipFragment fragment);
 }
--- app/src/main/java/com/codepath/blip/fragments/UserBlipFragment.java
+++ app/src/main/java/com/codepath/blip/fragments/UserBlipFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.codepath.blip.BlipApplication;
 import com.codepath.blip.clients.BackendClient;
@@ -51,7 +52,7 @@ public class UserBlipFragment extends BlipListFragment {
 
                     @Override
                     public void onError(Throwable e) {
-
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
